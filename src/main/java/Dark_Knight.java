a67d56a2cfc91d99ffacb4a8d7fd2a12932dd42d
LexiAKAtiff <lexiliutiff@gmail.com>
2026-01-20 18:07:49 +0800

    Level 0

---
+++ src/main/java/Dark_Knight.java
@@ -0,0 +1,10 @@
+public class Dark_Knight {
+    public static void main(String[] args) {
+        System.out.println("_".repeat(50));
+        System.out.println("Hello! I'm Dark Knight");
+        System.out.println("What can I do for you?");
+        System.out.println("_".repeat(50));
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_".repeat(50));
+    }
+}
