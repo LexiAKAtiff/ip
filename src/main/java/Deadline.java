9165905434640d2853fd4b12212ec06e0a03b8ab
LexiAKAtiff <lexiliutiff@gmail.com>
2026-01-30 14:03:03 +0800

    Teach to understand dates

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,21 +1,37 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
 
 public class Deadline extends Task{
-    String deadLine;
+    LocalDate by;
 
-    public Deadline(String description, String deadLine) {
+    ////for input format
+    private static final DateTimeFormatter INPUT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    //for output format
+    private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy", Locale.ENGLISH);
+
+    public Deadline(String description, String by) throws DarkKnightException{
         super(description);
-        this.deadLine = deadLine;
+        try {
+            this.by = LocalDate.parse(by, INPUT_FORMATTER);
+        } catch (DateTimeParseException e) {
+            throw new DarkKnightException("Invalid date format! Please use yyyy-MM-dd format (e.g., 2019-12-02)");
+        }
     }
 
-    public String getTime() {
-        return deadLine;
+    public LocalDate getTime() {
+        return by;
+    }
+
+
+    public String getByString() {
+        return this.by.format(OUTPUT_FORMATTER);
     }
 
     @Override
     public String toString() {
-        return ("[D]" + super.toString() + " (by: " + deadLine + ")");
+        return ("[D]" + super.toString() + " (by: " + getByString() + ")");
     }
 
 }
